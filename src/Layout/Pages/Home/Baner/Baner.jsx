ca83872f3df3651c32dadced7a4dc6d35891de5a
shsobur <shsoburhossen951@gmail.com>
2024-11-19 23:46:55 -0600

    Baner design completed

--- src/Layout/Pages/Home/Baner/Baner.jsx
+++ src/Layout/Pages/Home/Baner/Baner.jsx
@@ -3,11 +3,32 @@ import "./Baner.css";
 function Baner() {
   return (
     <>
-      <div>
-        Hello this is baner page
-      </div>
+      <section>
+        <div className="baner_top_main_container">
+          <div className="baner_bg_image_comtainer">
+            <img
+              src="https://i.ibb.co.com/F8hj7YT/domino-studio-164-6w-VEHf-I-unsplash-min.jpg"
+              alt="image"
+            />
+          </div>
+
+          <div className="baner_bg_image_over_flow_contaienr"></div>
+
+          <div className="baner_over_flow_text_content_container">
+            <h2>
+              FIND CLOTHES THAT <br /> MATCHES YOUR STYLE
+            </h2>
+            <p>
+              Browse through our diverse range of meticulously crafted garments,{" "}
+              <br /> designed to bring out your individuality and cater to your
+              sense of style.
+            </p>
+            <button>Shop Now</button>
+          </div>
+        </div>
+      </section>
     </>
-  )
+  );
 }
 
-export default Baner
+export default Baner;
